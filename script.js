dce3191661665f22c54a1b5df2945947be5e310f
Aditi Prusty <aditiamrut@gmail.com>
2023-08-12 00:23:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,268 @@
+var linkArray = [
+    { text: 'Admin', url: '#' },
+    { text: 'Change Password', url: '#' },
+    { text: 'Profile', url: '#c' },
+    { text: 'CLMS', url: '#' },
+    { text: 'eGreetings', url: '#' }
+      ];
+  
+      // Iterate through the array and generate list items with links
+      for (var i = 0; i < linkArray.length; i++) {
+        var linkItem = linkArray[i];
+        var listItem = '<li><a href="' + linkItem.url + '">' + linkItem.text + '</a></li>';
+        $('#card1').append(listItem);
+      }
+
+
+  var linkArray = [
+    { text: 'Admin', url: '#' },
+      { text: 'Change Password', url: '#' },
+      { text: 'Hrms', url: '#' },
+      { text: 'eGreetings', url: '#' }
+  ];
+
+  // Iterate through the array and generate list items with links
+  for (var i = 0; i < linkArray.length; i++) {
+    var linkItem = linkArray[i];
+    var listItem = '<li><a href="' + linkItem.url + '">' + linkItem.text + '</a></li>';
+    $('#card2').append(listItem);
+  }
+
+var linkArray = [
+  { text: 'Admin', url: '#' },
+  { text: 'CLMS', url: '#' },
+  { text: 'Change Password', url: '#' },
+  { text: 'Profile', url: '#c' },
+  { text: 'eGreetings', url: '#' }
+    ];
+
+    // Iterate through the array and generate list items with links
+    for (var i = 0; i < linkArray.length; i++) {
+      var linkItem = linkArray[i];
+      var listItem = '<li><a href="' + linkItem.url + '">' + linkItem.text + '</a></li>';
+      $('#card3').append(listItem);
+    }
+
+    var linkArray = [
+      { text: 'Admin', url: '#' },
+      { text: 'Change Password', url: '#' },
+      { text: 'Profile', url: '#c' },
+      { text: 'CLMS', url: '#' },
+      { text: 'eGreetings', url: '#' }
+        ];
+    
+        // Iterate through the array and generate list items with links
+        for (var i = 0; i < linkArray.length; i++) {
+          var linkItem = linkArray[i];
+          var listItem = '<li><a href="' + linkItem.url + '">' + linkItem.text + '</a></li>';
+          $('#card4').append(listItem);
+        }
+  
+  
+    var linkArray = [
+      { text: 'Admin', url: '#' },
+        { text: 'Change Password', url: '#' },
+        { text: 'Hrms', url: '#' },
+        { text: 'eGreetings', url: '#' }
+    ];
+  
+    // Iterate through the array and generate list items with links
+    for (var i = 0; i < linkArray.length; i++) {
+      var linkItem = linkArray[i];
+      var listItem = '<li><a href="' + linkItem.url + '">' + linkItem.text + '</a></li>';
+      $('#card5').append(listItem);
+    }
+  
+  var linkArray = [
+    { text: 'Admin', url: '#' },
+    { text: 'CLMS', url: '#' },
+    { text: 'Change Password', url: '#' },
+    { text: 'Profile', url: '#c' },
+    { text: 'eGreetings', url: '#' }
+      ];
+  
+      // Iterate through the array and generate list items with links
+      for (var i = 0; i < linkArray.length; i++) {
+        var linkItem = linkArray[i];
+        var listItem = '<li><a href="' + linkItem.url + '">' + linkItem.text + '</a></li>';
+        $('#card6').append(listItem);
+      }
+
+      var linkArray = [
+        { text: 'Admin', url: '#' },
+        { text: 'CLMS', url: '#' },
+        { text: 'Change Password', url: '#' },
+        { text: 'Profile', url: '#c' },
+        { text: 'eGreetings', url: '#' }
+          ];
+      
+          // Iterate through the array and generate list items with links
+          for (var i = 0; i < linkArray.length; i++) {
+            var linkItem = linkArray[i];
+            var listItem = '<li><a href="' + linkItem.url + '">' + linkItem.text + '</a></li>';
+            $('#card7').append(listItem);
+          }
+
+          var linkArray = [
+            { text: 'Admin', url: '#' },
+            { text: 'Change Password', url: '#' },
+            { text: 'Profile', url: '#c' },
+            { text: 'CLMS', url: '#' },
+            { text: 'eGreetings', url: '#' }
+              ];
+          
+              // Iterate through the array and generate list items with links
+              for (var i = 0; i < linkArray.length; i++) {
+                var linkItem = linkArray[i];
+                var listItem = '<li><a href="' + linkItem.url + '">' + linkItem.text + '</a></li>';
+                $('#card8').append(listItem);
+              }
+        
+        
+          var linkArray = [
+            { text: 'Admin', url: '#' },
+              { text: 'Change Password', url: '#' },
+              { text: 'Hrms', url: '#' },
+              { text: 'eGreetings', url: '#' }
+          ];
+        
+          // Iterate through the array and generate list items with links
+          for (var i = 0; i < linkArray.length; i++) {
+            var linkItem = linkArray[i];
+            var listItem = '<li><a href="' + linkItem.url + '">' + linkItem.text + '</a></li>';
+            $('#card9').append(listItem);
+          }
+        
+        var linkArray = [
+          { text: 'Admin', url: '#' },
+          { text: 'CLMS', url: '#' },
+          { text: 'Change Password', url: '#' },
+          { text: 'Profile', url: '#c' },
+          { text: 'eGreetings', url: '#' }
+            ];
+        
+            // Iterate through the array and generate list items with links
+            for (var i = 0; i < linkArray.length; i++) {
+              var linkItem = linkArray[i];
+              var listItem = '<li><a href="' + linkItem.url + '">' + linkItem.text + '</a></li>';
+              $('#card10').append(listItem);
+            }
+        
+            var linkArray = [
+              { text: 'Admin', url: '#' },
+              { text: 'Change Password', url: '#' },
+              { text: 'Profile', url: '#c' },
+              { text: 'CLMS', url: '#' },
+              { text: 'eGreetings', url: '#' }
+                ];
+            
+                // Iterate through the array and generate list items with links
+                for (var i = 0; i < linkArray.length; i++) {
+                  var linkItem = linkArray[i];
+                  var listItem = '<li><a href="' + linkItem.url + '">' + linkItem.text + '</a></li>';
+                  $('#card11').append(listItem);
+                }
+          
+          
+            var linkArray = [
+              { text: 'Admin', url: '#' },
+                { text: 'Change Password', url: '#' },
+                { text: 'Hrms', url: '#' },
+                { text: 'eGreetings', url: '#' }
+            ];
+          
+            // Iterate through the array and generate list items with links
+            for (var i = 0; i < linkArray.length; i++) {
+              var linkItem = linkArray[i];
+              var listItem = '<li><a href="' + linkItem.url + '">' + linkItem.text + '</a></li>';
+              $('#card12').append(listItem);
+            }
+
+
+
+
+            $('#i1').click(function() {
+              $('.cardd').removeClass('highlighted');
+              $('#capex').addClass('highlighted');
+            });
+
+            $('#i2').click(function() {
+              $('.cardd').removeClass('highlighted');
+              $('#cms').addClass('highlighted');
+            });
+
+            $('#i3').click(function() {
+              $('.cardd').removeClass('highlighted');
+              $('#hris').addClass('highlighted');
+            });
+
+            $('#i4').click(function() {
+              $('.cardd').removeClass('highlighted');
+              $('#cem').addClass('highlighted');
+            });
+
+            $('#i5').click(function() {
+              $('.cardd').removeClass('highlighted');
+              $('#eGreetings').addClass('highlighted');
+            });
+
+            $('#i6').click(function() {
+              $('.cardd').removeClass('highlighted');
+              $('#perquisites').addClass('highlighted');
+            });
+
+            $('#i7').click(function() {
+              $('.cardd').removeClass('highlighted');
+              $('#mis').addClass('highlighted');
+            });
+
+            $('#i8').click(function() {
+              $('.cardd').removeClass('highlighted');
+              $('#hr').addClass('highlighted');
+            });
+
+            $('#i9').click(function() {
+              $('.cardd').removeClass('highlighted');
+              $('#admin').addClass('highlighted');
+            });
+
+            $('#i10').click(function() {
+              $('.cardd').removeClass('highlighted');
+              $('#clms').addClass('highlighted');
+            });
+
+            $('#i11').click(function() {
+              $('.cardd').removeClass('highlighted');
+              $('#prm').addClass('highlighted');
+            });
+
+            $('#i12').click(function() {
+              $('.cardd').removeClass('highlighted');
+              $('#loans').addClass('highlighted');
+            });
+
+ 
+            
+
+
+            const search = () =>{
+              const searchbox = document.getElementById("search-input").value.toUpperCase();
+              const storeitems = document.getElementById("container")
+              const product = document.querySelectorAll(".product")
+              const pname = storeitems.getElementsByTagName("a")
+          
+              for(var i = 0; i < pname.length; i++){
+                  let match = product[i].getElementsByTagName('a')[0];
+          
+                  if(match){
+                      let textvalue = match.textContent || match.innerHTML
+          
+                      if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+                          product[i].style.display = "";
+                      }
+                      else{
+                          product[i].style.display = "none";
+                      }
+                  }
+              }
+          }
